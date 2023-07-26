565871df57c4a688644ae1156cbd2592642da151
tierrorofuego56 <122689590+tierrorofuego56@users.noreply.github.com>
2023-07-26 18:51:15 -0400

    Create UI.java

---
+++ UI.java
@@ -0,0 +1,10 @@
+public class UI {
+
+    public static String bankingIntro(){
+        return "1.] Add account\n2.] Search I.D Number\n3.] Display all I.D Numbers\n4.] Deposit Money\n5.] Withdraw Money";
+    }
+
+    public static String bankingType(){
+        return "Enter banking type:\n1.] Checkings\n2.] Savings\n3.] Money Market Account\n4.] Certificate of Deposit";
+    }
+}
